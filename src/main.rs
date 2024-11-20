36e61fefed42070d7749e1b345c2da7b429120b5
Mathieu Bes <bs.mathieu@hotmail.com>
2024-11-20 20:01:04 +0100

    move main alogithm in a run function to handle multiple project type in future

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
 mod file_handler;
 mod project;
 
-use std::collections::HashSet;
-
-use file_handler::{read_file_at_path, string_exists_in_multiline_text};
-use project::{
-    node_js::{read_deps_file, NodeProject},
-    Project,
-};
-use walkdir::WalkDir;
-
-use colored::*;
+use project::node_js;
 
 fn main() {
     println!(
@@ -18,59 +9,5 @@ fn main() {
         std::env::current_dir().unwrap().display()
     );
 
-    let mut project = NodeProject::default();
-    let deps_count = project.parse_deps(&read_deps_file());
-    println!("{deps_count} packages found in current project.");
-    let mut used_deps = HashSet::new();
-
-    let mut scanned_file_count = 0usize;
-    for entry in WalkDir::new(".") {
-        let entry = entry.unwrap();
-        if entry.path().is_dir() || !project.should_scan_file(entry.path().to_str().unwrap()) {
-            continue;
-        }
-
-        scanned_file_count += 1;
-
-        let buf = read_file_at_path(entry.path().to_str().unwrap()).unwrap();
-        let mut used_deps_in_file = Vec::new();
-
-        for dep_name in project.deps.iter() {
-            if string_exists_in_multiline_text(dep_name, &buf) {
-                used_deps_in_file.push(dep_name);
-            }
-        }
-
-        let total_unused_deps_count = deps_count - used_deps.len();
-
-        let print_str = format!(
-            "==============================
-> File : {}
-> Deps found : {:?}
-> Unused deps count : {}
-==============================",
-            entry.path().display(),
-            used_deps_in_file,
-            total_unused_deps_count
-        );
-
-        if used_deps_in_file.is_empty() {
-            println!("{}", print_str.red());
-        } else {
-            println!("{}", print_str);
-        }
-
-        for dep_name in used_deps_in_file.into_iter() {
-            used_deps.insert(dep_name);
-        }
-    }
-
-    for dep_name in project.deps.iter() {
-        if !used_deps.contains(dep_name) {
-            println!("Not used : {}", dep_name.red());
-        }
-    }
-
-    println!("{} files scanned", scanned_file_count);
-    println!("{} unused deps", deps_count - used_deps.len());
+    node_js::run();
 }
--- src/project/node_js.rs
+++ src/project/node_js.rs
@@ -1,10 +1,74 @@
-use std::{collections::HashMap, env, fs::File};
+use std::{
+    collections::{HashMap, HashSet},
+    env,
+    fs::File,
+};
 
-use crate::file_handler::read_file;
+use crate::file_handler::{read_file, read_file_at_path, string_exists_in_multiline_text};
 
 use super::Project;
 
+use colored::Colorize;
 use serde::Deserialize;
+use walkdir::WalkDir;
+
+pub fn run() {
+    let mut project = NodeProject::default();
+    let deps_count = project.parse_deps(&read_deps_file());
+    println!("{deps_count} packages found in current project.");
+    let mut used_deps = HashSet::new();
+
+    let mut scanned_file_count = 0usize;
+    for entry in WalkDir::new(".") {
+        let entry = entry.unwrap();
+        if entry.path().is_dir() || !project.should_scan_file(entry.path().to_str().unwrap()) {
+            continue;
+        }
+
+        scanned_file_count += 1;
+
+        let buf = read_file_at_path(entry.path().to_str().unwrap()).unwrap();
+        let mut used_deps_in_file = Vec::new();
+
+        for dep_name in project.deps.iter() {
+            if string_exists_in_multiline_text(dep_name, &buf) {
+                used_deps_in_file.push(dep_name);
+            }
+        }
+
+        let total_unused_deps_count = deps_count - used_deps.len();
+
+        let print_str = format!(
+            "==============================
+> File : {}
+> Deps found : {:?}
+> Unused deps count : {}
+==============================",
+            entry.path().display(),
+            used_deps_in_file,
+            total_unused_deps_count
+        );
+
+        if used_deps_in_file.is_empty() {
+            println!("{}", print_str.red());
+        } else {
+            println!("{}", print_str);
+        }
+
+        for dep_name in used_deps_in_file.into_iter() {
+            used_deps.insert(dep_name);
+        }
+    }
+
+    for dep_name in project.deps.iter() {
+        if !used_deps.contains(dep_name) {
+            println!("Not used : {}", dep_name.red());
+        }
+    }
+
+    println!("{} files scanned", scanned_file_count);
+    println!("{} unused deps", deps_count - used_deps.len());
+}
 
 const DEPS_FILE: &str = "package.json";
 
